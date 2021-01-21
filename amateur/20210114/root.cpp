9e3519b163c7035c0b9d1ed870679ab43576e1aa
deardeer7 <deardeer0091@163.com>
2021-01-21 22:00:38 +0800

    clean

--- amateur/20210114/root.cpp
+++ amateur/20210114/root.cpp
@@ -7,9 +7,25 @@ using namespace std;
 #define endl '\n'
 typedef long long ll;
 
-string root(string& s) {
-
-    
+// 求最小重复子串，没有返回”“
+string root(string s) {
+    int slen = s.size();
+    for(int i = 1; i <= s.size(); ++i) {
+        string t = s.substr(i);
+        int tlen = t.size();
+        if(slen % tlen != 0) continue;
+        bool ok = true;
+        for(int i = 0; i < slen; i += tlen) {
+            if(s.substr(i, tlen) != t) {
+                ok = false;
+                break;
+            }
+        }
+        if(ok) {
+            return t;
+        }
+    }
+    return "";
 }
 
 int main() {
